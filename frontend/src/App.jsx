12a70a49f7aa1ece3f0826f50bb21d67cb0d01b9
girish1802 <142975821+girish1802@users.noreply.github.com>
2025-02-09 00:04:09 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,12 @@ import Footer from "./components/footer/Footer";
 import Funding from "./components/funding/Funding";
 import IncubationSupport from "./components/incubation-support/IncubationSupport";
 import Services from "./components/services/Services";
-<<<<<<< HEAD
+
 import ServiceFacilities from "./components/services/ServiceFacilities";
 
-=======
+
 import Fellowships from "./components/funding/Fellowships";
->>>>>>> 271e40d455097e4faed3ea39165e57762fa75698
+
 
 function App() {
   return (
@@ -26,11 +26,9 @@ function App() {
           <Route path="/incubation-support" element={<IncubationSupport />} />
           <Route path="/funding" element={<Funding />} />
           <Route path="/services" element={<Services />} />
-<<<<<<< HEAD
           <Route path="/services/scientific-services-and-facilities" element={<ServiceFacilities />} />
-=======
           <Route path="/funding/fellowships" element={<Fellowships />} />
->>>>>>> 271e40d455097e4faed3ea39165e57762fa75698
+
         </Routes>
         <Footer />
       </BrowserRouter>
