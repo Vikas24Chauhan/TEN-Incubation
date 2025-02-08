0a24703c859b25eef290bc9959c6315646be712e
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-02-08 09:50:48 +0530

    navBar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
@@ -8,14 +9,14 @@ import Funding from "./components/funding/Funding";
 import IncubationSupport from "./components/incubation-support/IncubationSupport";
 import Services from "./components/services/Services";
 
-
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about-us" element={<About />} />
           <Route path="/incubation-support" element={<IncubationSupport />} />
           <Route path="/funding" element={<Funding />} />
           <Route path="/services" element={<Services />} />
