2aaf1c3cd5a1c52181a72ce3cbdfb6009e6cbe8e
Ansh <agnihotriansh07@gmail.com>
2025-02-10 17:14:56 +0530

    preIncubationPageCard

--- frontend/src/components/incubation-support/Card.jsx
+++ frontend/src/components/incubation-support/Card.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function Card(props){
     return(
         <>
@@ -12,21 +11,3 @@ export default function Card(props){
         </>
     )
 }
-=======
-export default function Card(props) {
-  console.log(props);
-  return (
-    <>
-      <div className="incubationSupport-card">
-        <img src={props.image.src} alt={props.image.alt} />
-        <div className="incubationSupport-card-content">
-          <h2 className="incubationSupport-card-title">{props.title}</h2>
-          <p className="incubationSupport-card-description">
-            {props.description}
-          </p>
-        </div>
-      </div>
-    </>
-  );
-}
->>>>>>> f65ded480205f50dbef9af78d366076444a7d3b6
