82d0f5ccc84c915afa20bf7e7872baf82bee7568
Ansh <agnihotriansh07@gmail.com>
2025-02-17 18:03:49 +0530

    SoftLandingFixed

--- src/components/incubation-support/SoftLandingProgram.jsx
+++ src/components/incubation-support/SoftLandingProgram.jsx
@@ -7,7 +7,7 @@ export default function SoftLandingProgram() {
                 <div className="softLandingProgram-header">
                     <h1>Soft Landing Program</h1>
                 </div>
-                <div className="content">
+                <div className="softLandingcontent">
                     <div className="incubationSupportnav">
                         <div class="in-page-nav">
                             <a class="back" href="/incubation-support">Back</a>
@@ -17,17 +17,17 @@ export default function SoftLandingProgram() {
                             <a class="" href="/incubation-support/softLandingProgram">Soft Landing Program</a>
                         </div>
                         <div className="main-content">
-                            <h3>Our soft landing program offers customized support for large companies looking to relocate their R&D and technology development operations to Pune, leveraging synergies with National Chemical Laboratory and regional research institutions, including access to essential services and expert referrals.</h3>
+                            <h3>Specialized assistance for big businesses wishing to move their technology development and research and development activities to Pune.</h3>
                             <hr />
                             <h4>Features of the program:</h4>
                             <ul>
-                                <li>Access to various services, high-end facilities and resources, depending upon eligibility and need of the company</li>
-                                <li>Advise and connects and referrals including referrals for HR, larger lab/office spaces etc</li>
+                                <li>Access to a range of services, upscale facilities, and resources based on the company's needs and eligibility</li>
+                                <li>Provide guidance, connections, and recommendations, such as those for HR, larger lab/office spaces, etc.</li>
                             </ul>
                             <h4>Eligibility</h4>
                             <ul>
-                                <li>Activities of the company are focused on R&D and technology development. Planned human resources emphasize research personnel.</li>
-                                <li>All Soft Landing requests involving existing/established/larger companies can be accepted depending up on the availability of facilities and resources. Venture Center has allotted only 10% of overall incubation space to assist entities under Soft Landing Program.</li>
+                                <li>The company's activities are concentrated on technological development and research and development. The focus of planned human resources is on research staff.</li>
+                                <li>Depending on the facilities and resources available, any Soft Landing request involving larger, more established, or current businesses may be approved. Just 10% of the total incubation space has been set aside by Venture Center to support organizations participating in the Soft Landing Program.</li>
                             </ul>
                             <h4>Terms, Conditions, and Pricing</h4>
                             <ul>
@@ -35,13 +35,13 @@ export default function SoftLandingProgram() {
                             <li className="categories">
                             <p><span className="highlight">Category 1:</span> At a premium of 50% of the listed prices to <strong>multinational companies* .</strong></p>
                             <p>
-                                The premium has been revised to 50% with effect from 1st August 2017 and will be applicable for all existing multi-national large establishments under category 1 of Soft Landing program currently in Venture Center and the new companies inducted after 1st August 2017. <br /><br />
-                                *A multinational company is defined as the one that has facilities and other assets in at least one country other than its home country. Such companies have offices and/or factories in different countries and usually have a centralized head office where they coordinate global management
+                            All current multinational large establishments under category 1 of the Soft Landing program that are presently in Venture Center, as well as any new companies hired after August 1, 2017, will be subject to the revised premium of 50%, which goes into effect on August 1st. <br /><br />
+                            *A corporation is considered multinational if it has assets and facilities in at least one nation outside of its home country. These businesses typically have a centralized head office where they coordinate worldwide administration, and they may have offices and/or factories spread throughout several nations.
                             </p>
                             <br />
                             <p><span className="highlight">Category 2:</span> At a premium of 25% of the listed prices to <strong>large established Indian companies.</strong></p>
                             <p>
-                                The premium category of 25 % has been introduced with effect from 24th January 2020 and will be applicable for all existing large Indian establishments under category 2 of the Soft Landing program currently in Venture Center and the new companies inducted after 24th January 2020.</p>
+                            With effect from January 24, 2020, the 25% premium category will be applicable to any significant Indian enterprises that are currently in Venture Center under category 2 of the Soft Landing program, as well as any new companies that are brought on board after that date.</p>
                                 </li>
                             </ul>
                             <ul>
@@ -51,24 +51,24 @@ export default function SoftLandingProgram() {
                                     <li>Dedicated full service office space</li>
                                     <li>Hot Desking service</li>
                                 </ul>
-                                <p style={{ padding: 0 }}> <strong> Note: </strong> The Bio-Starter package service as a part of dedicated lab space is not available to the companies under Soft program.</p>
+                                <p style={{ padding: 0 }}> <strong> Note: </strong> Companies under the Soft program are not eligible to use the Bio-Starter package service as part of dedicated lab space.</p>
                                 <li>
                                     Discounts: No discounts will be applicable on any of the Venture Center services under the Soft Landing program.
                                 </li>
                             </ul>
                             <h4>Payment:</h4>
                             <ul>
-                                <li>An advance payment needs to be made for all the services. The duration of advance payment depends on the service requested.</li>
-                                <li>Demand draft or local/ at-par cheque in the name of "Entrepreneurship Development Center" or direct deposit into the Venture Center's bank account. Online bank transfer options available. Bank details available here.</li>
+                                <li>For all services, a payment in advance is required. The requested service determines how long the advance payment must be made.</li>
+                                <li>"Entrepreneurship Development Center" can be paid by demand draft, local/at-par check, or direct transfer into the Venture Center's bank account. There are choices for online bank transfers.</li>
                             </ul>
                             <hr />
                             <h4> <strong> How to Apply</strong></h4>
                             <ul>
-                                <li>Please contact the Venture Center staff for preliminary discussions. This allows Venture Center staff to understand your needs and propose possible options</li>
-                                <li>Companies have to apply for each programs of interest separately</li>
-                                <li>Submit the application in person or by post or send scanned copy by email to the above address.</li>
-                                <li>The Venture Center staff has to table the request to the Incubator facilities and services oversight committee for approval before responding.</li>
-                                <li>The decision of Venture Center management shall be final regarding acceptance into the program.</li>
+                                <li>For initial conversations, please get in touch with the Venture Center team. This enables the personnel at Venture Center to comprehend your problems and suggest potential solutions.</li>
+                                <li>Businesses must submit separate applications for each program of interest.</li>
+                                <li>Send a scanned copy of the application to the aforementioned address via email, postal mail, or in person.</li>
+                                <li>Before replying, the Venture Center staff must submit the request for approval to the Incubator facilities and services oversight committee.</li>
+                                <li>Regarding admission to the program, Venture Center management's choice is final.</li>
                             </ul>
                             <hr />
                             <h4><strong>Program Beneficiaries</strong></h4>
