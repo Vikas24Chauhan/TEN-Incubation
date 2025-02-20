07fdefa4d67a528aab072ee64e66e96b7123137e
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-02-20 11:28:56 +0530

    Incubation Support completed

--- src/components/incubation-support/SoftLandingProgram.jsx
+++ src/components/incubation-support/SoftLandingProgram.jsx
@@ -1,106 +1,194 @@
-import "./SoftLandingProgram.css"
+import "./SoftLandingProgram.css";
+import { NavLink } from "react-router-dom";
 
 export default function SoftLandingProgram() {
-    return (
-        <>
-            <div className="softLandingProgram-content">
-                <div className="softLandingProgram-header">
-                    <h1>Soft Landing Program</h1>
-                </div>
-                <div className="softLandingcontent">
-                    <div className="incubationSupportnav">
-                        <div class="in-page-nav">
-                            <a class="back" href="/incubation-support">Back</a>
-                            <a class="current" href="/incubation-support/preIncubation">Pre-incubation</a>
-                            <a class="" href="/incubation-support/virtualIncubation">Virtual Incubation</a>
-                            <a class="" href="/incubation-support/residentIncubation">Resident Incubation</a>
-                            <a class="" href="/incubation-support/softLandingProgram">Soft Landing Program</a>
-                        </div>
-                        <div className="main-content">
-                            <h3>Specialized assistance for big businesses wishing to move their technology development and research and development activities to Pune.</h3>
-                            <hr />
-                            <h4>Features of the program:</h4>
-                            <ul>
-                                <li>Access to a range of services, upscale facilities, and resources based on the company's needs and eligibility</li>
-                                <li>Provide guidance, connections, and recommendations, such as those for HR, larger lab/office spaces, etc.</li>
-                            </ul>
-                            <h4>Eligibility</h4>
-                            <ul>
-                                <li>The company's activities are concentrated on technological development and research and development. The focus of planned human resources is on research staff.</li>
-                                <li>Depending on the facilities and resources available, any Soft Landing request involving larger, more established, or current businesses may be approved. Just 10% of the total incubation space has been set aside by Venture Center to support organizations participating in the Soft Landing Program.</li>
-                            </ul>
-                            <h4>Terms, Conditions, and Pricing</h4>
-                            <ul>
-                                <li>Under Soft landing program the Venture Center services are offered at a premium price point as compared to the prices listed for small entrepreneurs and start-ups. The premium is applicable under two categories.</li>
-                            <li className="categories">
-                            <p><span className="highlight">Category 1:</span> At a premium of 50% of the listed prices to <strong>multinational companies* .</strong></p>
-                            <p>
-                            All current multinational large establishments under category 1 of the Soft Landing program that are presently in Venture Center, as well as any new companies hired after August 1, 2017, will be subject to the revised premium of 50%, which goes into effect on August 1st. <br /><br />
-                            *A corporation is considered multinational if it has assets and facilities in at least one nation outside of its home country. These businesses typically have a centralized head office where they coordinate worldwide administration, and they may have offices and/or factories spread throughout several nations.
-                            </p>
-                            <br />
-                            <p><span className="highlight">Category 2:</span> At a premium of 25% of the listed prices to <strong>large established Indian companies.</strong></p>
-                            <p>
-                            With effect from January 24, 2020, the 25% premium category will be applicable to any significant Indian enterprises that are currently in Venture Center under category 2 of the Soft Landing program, as well as any new companies that are brought on board after that date.</p>
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>The premium is applicable up-front on services listed below,</li>
-                                <ul type="square">
-                                    <li>Dedicated Lab space</li>
-                                    <li>Dedicated full service office space</li>
-                                    <li>Hot Desking service</li>
-                                </ul>
-                                <p style={{ padding: 0 }}> <strong> Note: </strong> Companies under the Soft program are not eligible to use the Bio-Starter package service as part of dedicated lab space.</p>
-                                <li>
-                                    Discounts: No discounts will be applicable on any of the Venture Center services under the Soft Landing program.
-                                </li>
-                            </ul>
-                            <h4>Payment:</h4>
-                            <ul>
-                                <li>For all services, a payment in advance is required. The requested service determines how long the advance payment must be made.</li>
-                                <li>"Entrepreneurship Development Center" can be paid by demand draft, local/at-par check, or direct transfer into the Venture Center's bank account. There are choices for online bank transfers.</li>
-                            </ul>
-                            <hr />
-                            <h4> <strong> How to Apply</strong></h4>
-                            <ul>
-                                <li>For initial conversations, please get in touch with the Venture Center team. This enables the personnel at Venture Center to comprehend your problems and suggest potential solutions.</li>
-                                <li>Businesses must submit separate applications for each program of interest.</li>
-                                <li>Send a scanned copy of the application to the aforementioned address via email, postal mail, or in person.</li>
-                                <li>Before replying, the Venture Center staff must submit the request for approval to the Incubator facilities and services oversight committee.</li>
-                                <li>Regarding admission to the program, Venture Center management's choice is final.</li>
-                            </ul>
-                            <hr />
-                            <h4><strong>Program Beneficiaries</strong></h4>
-                            <strong>Current beneficiaries:</strong>
-                            <ul>
-                                <li>MFC Pvt. Ltd.</li>
-                                <li>Tarana Wireless India Pvt Ltd</li>
-                            </ul>
-                            <strong>Past beneficiaries:</strong>
-                            <ul>
-                                <li>Christian Hansen</li>
-                                <li>
-                                    D.A. Stuart India Private Limited (A Houghton Company)</li>
-                                <li>
-                                    Novozymes South Asia Pvt Ltd</li>
-                                <li>
-                                    Quintessence Fragrances Ltd</li>
-                                <li>
-                                    Battelle India</li>
-                                <li>
-                                    Systems Biology India Pvt Ltd</li>
-                                <li>
-                                    Avery Dennison's Research Center in Pune</li>
-                                <li>
-                                    DSM India</li>
-                                <li>
-                                    Indian Institute of Science Education and Research - Pune</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="softLandingProgram-div">
+        <div className="softLandingProgram-header">
+          <h1>Soft Landing Program</h1>
+        </div>
+
+        <div className="softLandingProgram-content">
+          <div className="preIncubation-navigation">
+            <NavLink to="/incubation-support">Back</NavLink>
+            <NavLink to="/incubation-support/preIncubation">
+              Pre Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/virtualIncubation">
+              Virtual Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/residentIncubation">
+              Resident Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/softLandingProgram">
+              Soft Landing Program
+            </NavLink>
+          </div>
+
+          <div className="softLandingProgram-main-content">
+            <h3>
+              Specialized assistance for big businesses wishing to move their
+              technology development and research and development activities to
+              Pune.
+            </h3>
+            <hr />
+            <h4>Features of the program:</h4>
+            <ul>
+              <li>
+                Access to a range of services, upscale facilities, and resources
+                based on the company's needs and eligibility
+              </li>
+              <li>
+                Provide guidance, connections, and recommendations, such as
+                those for HR, larger lab/office spaces, etc.
+              </li>
+            </ul>
+            <h4>Eligibility</h4>
+            <ul>
+              <li>
+                The company's activities are concentrated on technological
+                development and research and development. The focus of planned
+                human resources is on research staff.
+              </li>
+              <li>
+                Depending on the facilities and resources available, any Soft
+                Landing request involving larger, more established, or current
+                businesses may be approved. Just 10% of the total incubation
+                space has been set aside by Venture Center to support
+                organizations participating in the Soft Landing Program.
+              </li>
+            </ul>
+            <h4>Terms, Conditions, and Pricing</h4>
+            <ul>
+              <li>
+                Under Soft landing program the Venture Center services are
+                offered at a premium price point as compared to the prices
+                listed for small entrepreneurs and start-ups. The premium is
+                applicable under two categories.
+              </li>
+              <li className="categories">
+                <p>
+                  <span className="highlight">Category 1:</span> At a premium of
+                  50% of the listed prices to{" "}
+                  <strong>multinational companies* .</strong>
+                </p>
+                <p>
+                  All current multinational large establishments under category
+                  1 of the Soft Landing program that are presently in Venture
+                  Center, as well as any new companies hired after August 1,
+                  2017, will be subject to the revised premium of 50%, which
+                  goes into effect on August 1st. <br />
+                  <br />
+                  *A corporation is considered multinational if it has assets
+                  and facilities in at least one nation outside of its home
+                  country. These businesses typically have a centralized head
+                  office where they coordinate worldwide administration, and
+                  they may have offices and/or factories spread throughout
+                  several nations.
+                </p>
+                <br />
+                <p>
+                  <span className="highlight">Category 2:</span> At a premium of
+                  25% of the listed prices to{" "}
+                  <strong>large established Indian companies.</strong>
+                </p>
+                <p>
+                  With effect from January 24, 2020, the 25% premium category
+                  will be applicable to any significant Indian enterprises that
+                  are currently in Venture Center under category 2 of the Soft
+                  Landing program, as well as any new companies that are brought
+                  on board after that date.
+                </p>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                The premium is applicable up-front on services listed below,
+              </li>
+              <ul type="square">
+                <li>Dedicated Lab space</li>
+                <li>Dedicated full service office space</li>
+                <li>Hot Desking service</li>
+              </ul>
+              <p style={{ padding: 0 }}>
+                {" "}
+                <strong> Note: </strong> Companies under the Soft program are
+                not eligible to use the Bio-Starter package service as part of
+                dedicated lab space.
+              </p>
+              <li>
+                Discounts: No discounts will be applicable on any of the Venture
+                Center services under the Soft Landing program.
+              </li>
+            </ul>
+            <h4>Payment:</h4>
+            <ul>
+              <li>
+                For all services, a payment in advance is required. The
+                requested service determines how long the advance payment must
+                be made.
+              </li>
+              <li>
+                "Entrepreneurship Development Center" can be paid by demand
+                draft, local/at-par check, or direct transfer into the Venture
+                Center's bank account. There are choices for online bank
+                transfers.
+              </li>
+            </ul>
+            <hr />
+            <h4>
+              {" "}
+              <strong> How to Apply</strong>
+            </h4>
+            <ul>
+              <li>
+                For initial conversations, please get in touch with the Venture
+                Center team. This enables the personnel at Venture Center to
+                comprehend your problems and suggest potential solutions.
+              </li>
+              <li>
+                Businesses must submit separate applications for each program of
+                interest.
+              </li>
+              <li>
+                Send a scanned copy of the application to the aforementioned
+                address via email, postal mail, or in person.
+              </li>
+              <li>
+                Before replying, the Venture Center staff must submit the
+                request for approval to the Incubator facilities and services
+                oversight committee.
+              </li>
+              <li>
+                Regarding admission to the program, Venture Center management's
+                choice is final.
+              </li>
+            </ul>
+            <hr />
+            <h4>
+              <strong>Program Beneficiaries</strong>
+            </h4>
+            <strong>Current beneficiaries:</strong>
+            <ul>
+              <li>MFC Pvt. Ltd.</li>
+              <li>Tarana Wireless India Pvt Ltd</li>
+            </ul>
+            <strong>Past beneficiaries:</strong>
+            <ul>
+              <li>Christian Hansen</li>
+              <li>D.A. Stuart India Private Limited (A Houghton Company)</li>
+              <li>Novozymes South Asia Pvt Ltd</li>
+              <li>Quintessence Fragrances Ltd</li>
+              <li>Battelle India</li>
+              <li>Systems Biology India Pvt Ltd</li>
+              <li>Avery Dennison's Research Center in Pune</li>
+              <li>DSM India</li>
+              <li>Indian Institute of Science Education and Research - Pune</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/incubation-support/VirtualIncubation.jsx
+++ src/components/incubation-support/VirtualIncubation.jsx
@@ -1,68 +1,139 @@
-import "./VirtualIncubation.css"
+import "./VirtualIncubation.css";
+import { NavLink } from "react-router-dom";
 
 export default function VirtualIncubation() {
-    return (
-        <>
-            <div className="virtualIncubation-content">
-                <div className="virtualIncubation-header">
-                    <h1>Virtual Incubation</h1>
-                </div>
-                <div className="content">
-                    <div className="incubationSupportnav">
-                        <div class="in-page-nav">
-                            <a class="back" href="/incubation-support">Back</a>
-                            <a class="current" href="/incubation-support/preIncubation">Pre-incubation</a>
-                            <a class="" href="/incubation-support/virtualIncubation">Virtual Incubation</a>
-                            <a class="" href="/incubation-support/residentIncubation">Resident Incubation</a>
-                            <a class="" href="/incubation-support/softLandingProgram">Soft Landing Program</a>
-                        </div>
-                        <div className="virtual-page-content">
-                            <article>
-                                <h4><strong>Venture Center's Associate Incubatee Program (AIP) is designed for science-based entrepreneurial businesses that are not physically housed on our campus.</strong></h4>
-                                <hr />
-                                <h3>Membership Criteria & Eligibility</h3>
-                                <p>Members of Venture Center's Associate Incubatee Program are chosen through a selective admissions process. Venture Center aims to make sure that every Associate Incubatee Program participant is connected to a science-led, ethically-driven business that has access to high-quality services, pertinent resources, and extensive networks. In this case, qualifying requirements consist of:</p>
-                                <h5>Selective Admission Criteria</h5>
-                                <ul>
-                                    <li>Eligible parties include individuals, start-ups, and established businesses preparing to launch a new project.</li>
-                                    <li>Not-for-profit businesses and social entrepreneurs are also qualified.</li>
-                                    <li>In keeping with the "innovation" ethos of the NCL Innovation Park, business must be grounded in research.</li>
-                                    <li>Considerations include EHS and ethical factors, and the company should be relatively early stage.</li>
-                                    <li>Ineligibility for Residents:</li>
-                                    <ul><li>The program is not available to resident incubatees.</li>
-                                    </ul>
-                                    <li>Tax and Funding Considerations: </li>
-                                    <ul>
-                                        <li>Members do not qualify for service tax benefits or government concessions for resident incubate companies.</li>
-                                        <li>Funding programs may be reserved for resident incubatees, and participation for Associate Incubatee Program (AIP) members is subject to the decision of Scheme Managers and the Investment Committee.</li>
-                                    </ul>
-                                </ul>
+  return (
+    <>
+      <div className="virtualIncubation-div">
+        <div className="virtualIncubation-header">
+          <h1>Virtual Incubation</h1>
+        </div>
 
-                                <p><strong>By signing up for any of the programs listed below, a candidate can become an Associate Incubatee.</strong></p>
+        <div className="virtualIncubation-content">
+          <div className="virtualIncubation-navigation">
+            <NavLink to="/incubation-support">Back</NavLink>
+            <NavLink to="/incubation-support/preIncubation">
+              Pre Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/virtualIncubation">
+              Virtual Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/residentIncubation">
+              Resident Incubation
+            </NavLink>
+            <NavLink to="/incubation-support/softLandingProgram">
+              Soft Landing Program
+            </NavLink>
+          </div>
 
-                                <h5>1.Eklavya</h5>
-                                <p>Program for Mentoring and Advisory Assistance with Membership in the Library and IP Services.</p>
-                                <h5>2. Include Program</h5>
-                                <p>In partnership with CSIR-NCL and CSIR-800, Venture Center's social enterprise program is called INCLUDE. According to Venture Center, social enterprises are businesses that cater to societal needs, especially those of marginalized groups. They place a strong emphasis on for-profit financial sustainability, winning over stakeholders and investors with their social impact. Additionally, the initiative supports inclusive innovation by advancing technologies that realize Dr. RA Mashelkar's "More from Less for More" concept.</p>
+          <div className="virtualIncubation-page-content">
+            <article>
+              <h4>
+                <strong>
+                  Venture Center's Associate Incubatee Program (AIP) is designed
+                  for science-based entrepreneurial businesses that are not
+                  physically housed on our campus.
+                </strong>
+              </h4>
+              <hr />
+              <h3>Membership Criteria & Eligibility</h3>
+              <p>
+                Members of Venture Center's Associate Incubatee Program are
+                chosen through a selective admissions process. Venture Center
+                aims to make sure that every Associate Incubatee Program
+                participant is connected to a science-led, ethically-driven
+                business that has access to high-quality services, pertinent
+                resources, and extensive networks. In this case, qualifying
+                requirements consist of:
+              </p>
+              <h5>Selective Admission Criteria</h5>
+              <ul>
+                <li>
+                  Eligible parties include individuals, start-ups, and
+                  established businesses preparing to launch a new project.
+                </li>
+                <li>
+                  Not-for-profit businesses and social entrepreneurs are also
+                  qualified.
+                </li>
+                <li>
+                  In keeping with the "innovation" ethos of the NCL Innovation
+                  Park, business must be grounded in research.
+                </li>
+                <li>
+                  Considerations include EHS and ethical factors, and the
+                  company should be relatively early stage.
+                </li>
+                <li>Ineligibility for Residents:</li>
+                <ul>
+                  <li>The program is not available to resident incubatees.</li>
+                </ul>
+                <li>Tax and Funding Considerations: </li>
+                <ul>
+                  <li>
+                    Members do not qualify for service tax benefits or
+                    government concessions for resident incubate companies.
+                  </li>
+                  <li>
+                    Funding programs may be reserved for resident incubatees,
+                    and participation for Associate Incubatee Program (AIP)
+                    members is subject to the decision of Scheme Managers and
+                    the Investment Committee.
+                  </li>
+                </ul>
+              </ul>
 
-                                <p>In addition to discounts on scientific facilities and infrastructure support, INCLUDE members have access to the Eklavya Service.</p>
-                                <p>By using Address Service, which allows businesses to use the "NCL Innovation Park address," applicants can also become associate incubators. This covers basic reception services, email notifications, mailboxes, and postal addresses. The COO has the final say about this service.</p>
+              <p>
+                <strong>
+                  By signing up for any of the programs listed below, a
+                  candidate can become an Associate Incubatee.
+                </strong>
+              </p>
 
-                            </article>
-                            <aside>
-                                <div>
-                                    <p><strong>Incubation Team</strong></p>
-                                    <p className="email">Email: <a href="#">managerincubator@venturecenter.co.in</a>
-                                        <br />
-                                        <br />
-                                        Phone: <a href="#">+91 9172232211</a></p>
-                                </div>
-                            </aside>
-                        </div>
+              <h5>1.Eklavya</h5>
+              <p>
+                Program for Mentoring and Advisory Assistance with Membership in
+                the Library and IP Services.
+              </p>
+              <h5>2. Include Program</h5>
+              <p>
+                In partnership with CSIR-NCL and CSIR-800, Venture Center's
+                social enterprise program is called INCLUDE. According to
+                Venture Center, social enterprises are businesses that cater to
+                societal needs, especially those of marginalized groups. They
+                place a strong emphasis on for-profit financial sustainability,
+                winning over stakeholders and investors with their social
+                impact. Additionally, the initiative supports inclusive
+                innovation by advancing technologies that realize Dr. RA
+                Mashelkar's "More from Less for More" concept.
+              </p>
 
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+              <p>
+                In addition to discounts on scientific facilities and
+                infrastructure support, INCLUDE members have access to the
+                Eklavya Service.
+              </p>
+              <p>
+                By using Address Service, which allows businesses to use the
+                "NCL Innovation Park address," applicants can also become
+                associate incubators. This covers basic reception services,
+                email notifications, mailboxes, and postal addresses. The COO
+                has the final say about this service.
+              </p>
+            </article>
+            <aside>
+              <div>
+                <p>
+                  <strong>Incubation Team</strong>
+                </p>
+                <p className="email">
+                  Email: <a href="#">info@entrepreneurshipnetwork.net</a>
+                </p>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
